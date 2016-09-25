32e5c40dcde111eb2ef1213286bb7ee6612a44e9
Hans-Kristian <hkbruvold@gmail.com>
2016-09-25 16:52:07 +0200

    Moved ProblemState class to tools.py

--- proj1/nqueensts.py
+++ proj1/nqueensts.py
@@ -1,35 +1,6 @@
 import math, random, time, sys
 from tools import *
 
-class ProblemState():
-    """Contains information dependent on problem size to be looked up.
-    Should not be changed after initialization."""
-    def __init__(self, size):
-        self.size = size
-        self.comb = ProblemState.nC2List(size + 1)
-        self.target = self.comb[size]
-        self.diagCoords = ProblemState.diagonalCoordinates(size)
-
-    def nC2List(n):
-        """List of n choose 2 from 0 to n"""
-        l = []
-        for i in range(n):
-            l.append(int(i*(i-1)/2))
-        return(l)
-
-    def diagonalCoordinates(size):
-        """Returns a lookup table of diagonal coordinates for each board square. In
-        this context a diagonal coordinate is the diagonal number for the sqare, both
-        from NW to SE and SW to NE, ordered from west to east. Coordinate location in
-        the table is given by standard column and rom number."""
-        diagCoords = [[[0 for i in range(2)] for j in range(size)] for k in range(size)]
-        for col in range(size):
-            for row in range(size):
-                #NW to SE diagonal
-                diagCoords[col][row][0] = col - row + size - 1
-                #SW to NE diagonal
-                diagCoords[col][row][1] = col + row
-        return diagCoords
 
 class BoardState():
     def __init__(self, pS, **kwargs):
