96c7ec14007c6d15b7fc368c68faa1b344d7821b
Harald Husum <harald.husum@gmail.com>
2016-10-06 14:20:01 +0200

    ex1 complete

---
+++ proj2/ex1/taxi-random.py
@@ -0,0 +1,11 @@
+import gym
+env = gym.make('Taxi-v1')
+observation = env.reset()
+for t in range(10000):
+    env.render()
+    print(observation)
+    action = env.action_space.sample()
+    observation, reward, done, info = env.step(action)
+    if done:
+        print("Episode finished after {} timesteps".format(t+1))
+        break
