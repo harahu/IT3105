fd21431befe0ea9f0249cdf4e349122dec23db98
Hans-Kristian <hkbruvold@gmail.com>
2016-10-18 14:39:36 +0200

    Added some colors

--- proj2/ex3/print_q_func.py
+++ proj2/ex3/print_q_func.py
@@ -1,14 +1,34 @@
 import os, sys
 import json
 
+# http://stackoverflow.com/questions/287871/print-in-terminal-with-colors-using-python
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 qfuncjson = open(sys.argv[1], 'r').read()
 qtable = json.loads(qfuncjson)
 
-table = ["SFFF",
-         "FHFH",
-         "FFFH",
-         "HFFG"]
+table = [['S','F','F','F'],
+         ['F','H','F','H'],
+         ['F','F','F','H'],
+         ['H','F','F','G']]
+
+table[0][0] = bcolors.OKGREEN + 'S' + bcolors.ENDC
+table[3][3] = bcolors.HEADER + 'G' + bcolors.ENDC
+
+for row in range(4):
+    for col in range(4):
+        if table[row][col] == 'F':
+            table[row][col] = bcolors.OKBLUE + 'F' + bcolors.ENDC
+        if table[row][col] == 'H':
+            table[row][col] = bcolors.FAIL + 'H' + bcolors.ENDC
 
 for row in range(4):
     # print 1. line
