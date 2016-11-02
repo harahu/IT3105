785dfb747884ed15d139d8bbbda8d7be20d7d1f4
Harald Husum <harald.husum@gmail.com>
2016-11-02 23:30:25 +0100

    filling out skeleton

--- proj3/tsp.py
+++ proj3/tsp.py
@@ -19,9 +19,18 @@ def get_problem_set(filename):
 
     return x, y
 
+def get_best_match(city, neuron_list):
+
+
+
 def main():
     x, y = get_problem_set('wi29.tsp')
     som_ring = [[random.random() for i in range(2)] for i in range(len(x))]
+    city = random.randrange(0, len(x))
+    match = get_best_match(city, som_ring) #index?
+    #train m and neighbours
+    #update parameters
+
 
 if __name__ == '__main__':
     main()
