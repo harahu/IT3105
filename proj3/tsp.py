a58dd47da55d4b47ce12bd23aca0e9fff8a82d68
Harald Husum <harald.husum@gmail.com>
2016-11-03 00:20:18 +0100

    changed input format

--- proj3/tsp.py
+++ proj3/tsp.py
@@ -7,29 +7,28 @@ def get_problem_set(filename):
 
     for i in range(len(inp)):
         inp[i] = inp[i].split(' ')[1:]
+        inp[i] = [float(j) for j in inp[i]]
 
-    x, y = [], []
+    max_x = max(city[0] for city in inp)
+    max_y = max(city[1] for city in inp)
 
-    for i in inp:
-        x.append(float(i[0]))
-        y.append(float(i[1]))
+    inp = [[city[0]/max_x, city[1]/max_y] for city in inp]
 
-    x = [float(i)/max(x) for i in x]
-    y = [float(i)/max(y) for i in y]
+    return inp
 
-    return x, y
-
-def get_best_match(city, neuron_list):
-
-
+def get_best_match_index(city, neuron_list):
+    best = 0
+    best_val = 0
 
 def main():
-    x, y = get_problem_set('wi29.tsp')
-    som_ring = [[random.random() for i in range(2)] for i in range(len(x))]
-    city = random.randrange(0, len(x))
-    match = get_best_match(city, som_ring) #index?
+    #initialization
+    cities = get_problem_set('wi29.tsp')
+    som_ring = [[random.random() for i in range(2)] for i in range(len(cities))]
+    city = random.randrange(0, len(cities))
+
+    match = get_best_match_index(city, som_ring) #index?
     #train m and neighbours
-    #update parameters
+    #update parame
 
 
 if __name__ == '__main__':
