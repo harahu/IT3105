9a5c9861918e020d11091512b2d7ff7460fdaba4
Harald Husum <harald.husum@gmail.com>
2016-11-02 17:47:17 +0100

    added main function

--- proj3/tsp.py
+++ proj3/tsp.py
@@ -1,19 +1,27 @@
-with open('wi29.tsp', 'r') as f:
-    inp = [line.rstrip('\n') for line in f]
-inp = inp[7:len(inp)-1]
+import random
 
-for i in range(len(inp)):
-    inp[i] = inp[i].split(' ')[1:]
+def get_problem_set(filename):    
+    with open(filename, 'r') as f:
+        inp = [line.rstrip('\n') for line in f]
+    inp = inp[7:len(inp)-1]
 
-x = []
-y = []
+    for i in range(len(inp)):
+        inp[i] = inp[i].split(' ')[1:]
 
-for i in inp:
-    x.append(float(i[0]))
-    y.append(float(i[1]))
+    x, y = [], []
 
-x = [float(i)/max(x) for i in x]
-y = [float(i)/max(y) for i in y]
+    for i in inp:
+        x.append(float(i[0]))
+        y.append(float(i[1]))
 
-print(x)
-print(y)
+    x = [float(i)/max(x) for i in x]
+    y = [float(i)/max(y) for i in y]
+
+    return x, y
+
+def main():
+    x, y = get_problem_set('wi29.tsp')
+    som_ring = [[random.random() for i in range(2)] for i in range(len(x))]
+
+if __name__ == '__main__':
+    main()
