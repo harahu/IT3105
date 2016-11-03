5aece0e33dd421e249d79ca4866573c1071cf73a
Hans-Kristian <hkbruvold@gmail.com>
2016-11-03 20:06:18 +0100

    Added functions to calculate total distance. Using more neurons than cities now.

--- proj3/tsp.py
+++ proj3/tsp.py
@@ -4,7 +4,7 @@ import matplotlib.animation as anim
 
 # for animation
 fig = plt.figure()
-ax = plt.axes(xlim=(0,1), ylim=(0,1))
+ax = plt.axes(xlim=(-0.2,1.2), ylim=(-0.2,1.2))
 cities_plt, = ax.plot([], [], 'ro', zorder=2, alpha=0.4)
 neurons_plt, = ax.plot([], [], 'yo-', zorder=1)
 
@@ -79,6 +79,38 @@ def get_best_match_index(city, neuron_list):
             
     return best
 
+# Returns list of the closest cities per neuron
+def get_closest_city_list(som_ring, cities):
+    closest_list = [set() for i in range(len(som_ring))]
+    
+    # Add closest city to each neuron
+    for neuron_index in range(len(som_ring)):
+        closest_city = get_best_match_index(som_ring[neuron_index], cities)
+        closest_list[neuron_index].add(closest_city)
+    
+    # Add closest neuron for each city (if more than one city has the same neuron as closest)
+    for city_index in range(len(cities)):
+        closest_neuron = get_best_match_index(cities[city_index], som_ring)
+        closest_list[closest_neuron].add(city_index)
+    
+    return closest_list
+
+def calculate_total_distance(som_ring, cities, raw_cities):
+    total_distance = 0
+    closest_list = [list(cty) for cty in get_closest_city_list(som_ring, cities)]
+    
+    processed_cities = [closest_list[0][0]]
+    last_city = closest_list[0][0]
+    for city_list in closest_list:
+        for city in city_list:
+            if city not in processed_cities:
+                total_distance += euclidian_distance(raw_cities[city], raw_cities[last_city])
+                last_city = city
+    
+    total_distance += euclidian_distance(raw_cities[processed_cities[0]], raw_cities[last_city])
+    
+    return total_distance
+    
 def train(weight, city, alpha, discount):
     weight[0] += alpha*discount*(city[0]-weight[0])
     weight[1] += alpha*discount*(city[1]-weight[1])
@@ -88,7 +120,7 @@ def main():
     raw_cities, cities = get_problem_set('wi29.tsp')
     #som_ring = [[random.random() for i in range(2)] for i in range(len(cities))]
     som_ring = []
-    for i in range(len(cities)):
+    for i in range(len(cities)*2):
         tetha = i / len(cities) * 2 * math.pi
         som_ring.append([math.cos(tetha)/2 + 0.5, math.sin(tetha)/2 + 0.5])
     eta = 0.8
@@ -121,12 +153,14 @@ def main():
         
         if i % frame_step == 0:
             add_anim(neurons_data, som_ring)
+            print(calculate_total_distance(som_ring, cities, raw_cities))
     
     #plot_som_tsp(cities, som_ring)
     
     init_anim(cities)
     ani = anim.FuncAnimation(fig, animate, frames=len(neurons_data), fargs=(neurons_data,))
     
+    get_closest_city_list(som_ring, cities)
     plt.show()
 
 if __name__ == '__main__':
